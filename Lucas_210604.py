5036d81668e47a2bb0c36449a659f495507a7e4c
LucasMarin10 <84347671+LucasMarin10@users.noreply.github.com>
2021-06-05 00:10:33 -0300

    Add files via upload

---
+++ Lucas_210604.py
@@ -0,0 +1,56 @@
+#question 2
+fruits=["apple","banana","pear","orange","kiwi"]
+#question 3
+print(fruits[1])
+#question 4
+fruits.sort()
+#question 5
+print(fruits)
+#question 6
+fruits.insert(5,"apple")
+#question 7
+print(fruits)
+#question 8
+fruits.insert(0,"zucchini")
+#question 9
+print(fruits)
+#question 10
+afs=(sorted(fruits))
+#question 11
+print(fruits)
+print(afs)
+#question 12
+fruits.insert(0,"strawberry")
+#question 13
+print(fruits) 
+print(afs)
+#LOOPS
+#question 14
+games=["Minecraft","Subnautica","Brawlhalla"]
+#question 15
+print(games)
+#question 16
+for gam in games:
+    print(gam)
+#question 17
+for gam in games:
+    print(gam+" "+gam)
+#question 18
+for gam in games:
+    print(gam.upper())
+#question 19
+print("This is my list:")
+for gam in games:
+    print(gam)
+#question 20
+for gam in games:
+    print(gam)
+print("This was my list.")
+#question 21
+for gam in games:
+    print("This is one item of my list:")
+    print(gam)
+#question 22
+for gam in games:
+    print(gam)
+    print("This was one item of my list.")
